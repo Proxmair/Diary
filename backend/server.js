f92ddab5ff1ca3932f7b3ee0f34bfbd5a0c1753a
Proxmair <73277454+Proxmair@users.noreply.github.com>
2022-09-10 23:17:45 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,7 @@ import { app } from "./app.js";
 import dotenv from 'dotenv';
 import  {connectDatabase}  from "./config/database.js";
 
-if(process.env.NODE_ENV!=="PRODUCTION"){
-  dotenv.config({ path: "./backend/config/config.env" });
-}
-
+dotenv.config({ path: "./backend/config/config.env" });
 
 connectDatabase();
 
