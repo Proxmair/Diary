a6d8da2d4c2307417634cb8239b8ae25b94a8755
Proxmair <73277454+Proxmair@users.noreply.github.com>
2022-09-11 12:58:20 +0500

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDatabase = () => {
   mongoose
-    .connect(process.env.DB_URL)
+    .connect("mongodb+srv://Umair:Umair123@cluster0.wmcfxow.mongodb.net/?retryWrites=true&w=majority")
     .then((c) => {
       console.log(`Mongodb connect to: ${c.connection.host}`);
     })
