5a64e9537db40ffe21d055b6231eb0099f76e155
Proxmair <73277454+Proxmair@users.noreply.github.com>
2022-09-12 15:37:58 +0500

    Add files via upload

---
+++ frontend/build/precache-manifest.ae656b04a6bac7a8511933f5e5a9120b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b87bd622e6b1f949c1ff",
+    "url": "/static/css/main.3addac54.chunk.css"
+  },
+  {
+    "revision": "b87bd622e6b1f949c1ff",
+    "url": "/static/js/main.b87bd622.chunk.js"
+  },
+  {
+    "revision": "c7f2382158c91063d3a8",
+    "url": "/static/js/1.c7f23821.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b6d84a33f8b3896e7e0fd605a6bce4f4",
+    "url": "/index.html"
+  }
+];
